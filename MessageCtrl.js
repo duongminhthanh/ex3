7abe42c1f568264decba3c75f6c6793153557ee0
duongminhthanh <duongminhthanh1999td@gmail.com>
2021-10-27 14:21:23 +0700

    update

---
+++ MessageCtrl.js
@@ -0,0 +1,28 @@
+module.exports = {
+    sendMessage: function (req,res){
+        let Data =req.body || '';
+        let resData = {};
+        resData.name =Data.name;
+        resData.email=Data.email;
+        res.subject=Data.subject;
+        resData.message=Data.message;
+        let returnData = {};
+        returnData.data=resData;
+        returnData.result = 'OK';
+        res.status(200);
+        res.contentType('json');
+        res.json(returnData);
+    },
+
+    getMessage: function (req,res){
+        let resData={};
+        resData.name="duong minh thanh";
+        resData.email="duongminhthanh1999td@gmail.com";
+        let returnData ={};
+        returnData.data=resData;
+        returnData.result='OK';
+        res.status(200);
+        res.contentType('json');
+        res.json(returnData);
+    }
+};
--- index.js
+++ index.js
@@ -1,7 +1,40 @@
-const express = require("express");
-const PORT = process.env.PORT || 3000;
+const bodyparse = require('body-parser');
+const express = require('express');
+const methodOverride = require('method-override');
+
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`);
+app.use(bodyparse.json({
+   limit:'5mb'
+}));
+app.use(bodyparse.json({
+   type:'application/vnd.api+json'
+}));
+app.use(bodyparse.urlencoded({
+   limit:'5mb',
+   extended:true
+}));
+app.use(methodOverride('X-HTTP-Method-Override'));
+
+require('./routes')(app);
+
+let server = require('http').createServer(app);
+
+
+server.listen(3000,function () {
+   console.log('Server app running on port 3000');
 });
+
+/*const http=require("http");
+const hostname='127.0.0.1';
+const  port=3000;
+
+const server = http.createServer((req,res)=>{
+   res.statusCode=200;
+   res.setHeader('Content-Type', 'text/plain');
+   res.end('Hello World');
+});
+
+server.listen(port,hostname,()=>{
+   console.log('server running at http://' +hostname + ':' + port + '/');
+});*/
